19f7f4da74e41183f7c0327ac1cdd0abcb0bb669
Bruno Silveira <bruno_felipelustosa@hotmail.com>
2023-05-31 11:28:45 -0300

    Carousel

---
+++ cinema/src/components/Carousel/Carousel.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+import './Carousel.css'
+
+const Carousel = ({images}) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    
+    const goToPrevious = () => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    };
+  
+    const goToNext = () => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    };
+
+    return (
+        <div className="carousel">
+            <button className="carousel-button" onClick={goToPrevious}>
+                &lt;
+            </button>
+            <img
+                className="carousel-image"
+                src={images[currentImageIndex]}
+                alt="Carousel Image"
+            />
+            <button className="carousel-button" onClick={goToNext}>
+                &gt;
+            </button>
+        </div>
+    )
+}
+
+export default Carousel
--- cinema/src/pages/Home/Home.js
+++ cinema/src/pages/Home/Home.js
@@ -1,8 +1,18 @@
 import './Home.css'
+import Carousel from '../../components/Carousel/Carousel'
 
 const Home = () => {
+    const images = [
+        './CarouselImages/buzz-poster.jpg',
+        './CarouselImages/gotg-poster.jpg',
+        './CarouselImages/fafx-poster.jpg',
+        './CarouselImages/hp-poster.jpg',
+    ]
+
   return (
-    <div>Home</div>
+    <div>
+        <Carousel images={images} />
+    </div>
   )
 }
 
